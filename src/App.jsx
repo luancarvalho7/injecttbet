1b2f64e4cdf48348f5dba186e989559d997e8e5a
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2024-01-07 22:16:07 +0000

    mmabet

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
   const [vipAccess, setVipAccess] = useState(false)
   const [loaded, setLoaded] = useState(false)
   const [housesData, setHousesData] = useState(data);
-  const [currentHouse, setCurrentHouse] = useState('PlayPix')
+  const [currentHouse, setCurrentHouse] = useState('MMAbet')
   const [currentSupplier, setCurrentSupplier] = useState('Spribe')
   const hasNavigatedAway = useRef(false); // flag variable
 
